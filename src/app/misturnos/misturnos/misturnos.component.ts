8c04d4b3f905c39669f90075705b938a2eeda42a
hernan <hernan-vilar@hotmail.com>
2022-06-18 19:03:53 -0300

    casiterminado

--- src/app/misturnos/misturnos/misturnos.component.ts
+++ src/app/misturnos/misturnos/misturnos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AgregarestadoturnoService } from 'src/app/services/agregarestadoturno.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { HorariosturnosService } from 'src/app/services/horariosturnos.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -14,7 +15,7 @@ export class MisturnosComponent implements OnInit {
   resenia:boolean = false;
   reseniaActual:any;
 
-  constructor(public auth:AuthService,private agregarestadoturno:AgregarestadoturnoService) 
+  constructor(public auth:AuthService,private agregarestadoturno:AgregarestadoturnoService,private hsturnos:HorariosturnosService) 
   {
     this.agregarestadoturno.getAll().valueChanges().subscribe(e=>{
       this.list = e;
@@ -60,6 +61,7 @@ export class MisturnosComponent implements OnInit {
         }
         data.estado = "cancelado";
         this.buscar(data);
+        this.quehago(data.dia,data.hora,data.minutos,data.correoEspecialista);
       }
     })
   }
@@ -78,4 +80,50 @@ export class MisturnosComponent implements OnInit {
     this.resenia  = !this.resenia;
   }
 
+  quehago(dia:any,hora:any,minutos:any,email:any)
+  {
+    let arraytercero:any = [];
+   let tip =  this.hsturnos.getAll().valueChanges().subscribe(e=>{
+    tip.unsubscribe();
+      for(let i = 0; i<e.length;i++)
+      {
+          arraytercero.push(e[i]);
+      }
+      for(let i = 0; i<arraytercero.length;i++)
+      {
+        if(arraytercero[i].email == email)
+        {
+      for(let z = 0; z<arraytercero[i].fechas.length;z++)
+      {
+        if(arraytercero[i].fechas[z].dia == dia)
+        {
+          for(let j = 0; j<arraytercero[i].fechas[z].horario.length;j++)
+          {  
+            
+            if(arraytercero[i].fechas[z].horario[j].hora == hora && arraytercero[i].fechas[z].horario[j].minutos == minutos)
+            {
+              arraytercero[i].fechas[z].horario[j].estado= 'habilitado';
+              console.log("ROMPIO");
+              
+              
+              this.hsturnos.getAll().query.get().then(e=>{
+                e.forEach((e:any)=>{
+                   if(arraytercero[i].email == e.val().email && arraytercero[i].especialidad == e.val().especialidad)
+                   {
+                    this.hsturnos.update(e.key,arraytercero[i])
+                   }
+                 }
+                 )
+               })
+               break;
+            }
+          }
+        }
+      }
+    }
+    }
+    })  
+    
+  
+  }
 }
