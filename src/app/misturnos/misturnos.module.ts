ca8635fea5f3637791f279c2d136cce7f8d3d567
hernan <hernan-vilar@hotmail.com>
2022-06-21 18:01:53 -0300

    poco

--- src/app/misturnos/misturnos.module.ts
+++ src/app/misturnos/misturnos.module.ts
@@ -4,11 +4,17 @@ import { CommonModule } from '@angular/common';
 import { MisturnosRoutingModule } from './misturnos-routing.module';
 import { MisturnosComponent } from './misturnos/misturnos.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TurnopacienteComponent } from './turnopaciente/turnopaciente.component';
+import { TurnoespecialistaComponent } from './turnoespecialista/turnoespecialista.component';
+import { TurnoadministradorComponent } from './turnoadministrador/turnoadministrador.component';
 
 
 @NgModule({
   declarations: [
-    MisturnosComponent
+    MisturnosComponent,
+    TurnopacienteComponent,
+    TurnoespecialistaComponent,
+    TurnoadministradorComponent
   ],
   imports: [
     CommonModule,
